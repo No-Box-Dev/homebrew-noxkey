b88dab07219201355ce19155f1d5e893f96f1db0
JasperNoBoxDev <Jasper@noboxdev.com>
2026-03-24 18:42:15 +0000

    Update noxkey cask to v0.8.5

--- Casks/noxkey.rb
+++ Casks/noxkey.rb
@@ -1,6 +1,6 @@
 cask "noxkey" do
-  version "0.8.4"
-  sha256 "c488b6b46c48eb1419706ff8ed10be2704ce69fde3f13c5e1f4fc26a86e6f2d9"
+  version "0.8.5"
+  sha256 "2e556fbe8e5f54fc9d1d7bc7c36aec757801cc4e3d47cbcf477b0e121b262377"
   url "https://releases.noxkey.ai/v#{version}/NoxKey-#{version}.dmg?source=homebrew"
   name "NoxKey"
   desc "macOS Keychain secret manager with Touch ID"
